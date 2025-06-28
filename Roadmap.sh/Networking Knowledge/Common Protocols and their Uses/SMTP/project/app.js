37ebeb4dc154e55691f1a3e276ac8dbf136832bd
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2025-06-28 16:28:39 -0300

    feature: async function to send an email

--- Roadmap.sh/Networking Knowledge/Common Protocols and their Uses/SMTP/project/app.js
+++ Roadmap.sh/Networking Knowledge/Common Protocols and their Uses/SMTP/project/app.js
@@ -2,25 +2,55 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+// the set variables below are for sending emails
+const nodemailer = require('nodemailer');
+const from = 'programeseufuturoti@gmail.com';
+const password = 'qnuptusceovbnryo';
 
-app.post('/send', (req, res) =>{
-    
-    let subject = req.body.subject;
-    let text = req.body.text;
-    let from = req.body.text;
-    let to = req.body.to;
-
-    res.json({message: 'Email is sending...'});
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: from,
+        pass: password
+    }
 });
 
+async function sendEmail(content) {
+    let emailContent = {
+        from: from,
+        to: content.to,
+        subject: content.subject,
+        text: content.text
+    }
 
+    try {
+        await transporter.sendMail(emailContent);
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
 
+app.use(express.json());
 
+app.post('/send', async (req, res) => {
 
+    let content = {
+        subject: req.body.subject,
+        to: req.body.to,
+        text: req.body.text
+    }
 
+    let response = await sendEmail(content);
 
+    if (response == true) {
+        res.json({ message: 'Email is sending...' });
+    } else {
+        res.status(500).json({ message: 'Failed to send email' });
+    }
+});
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
